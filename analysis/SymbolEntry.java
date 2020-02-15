321a756845f8cb327d997e28c837070755174d17
kunwithgithub <kunhuang@ucdavis.edu>
2020-02-15 14:06:11 -0800

    modified Symbol table and symbol entry

--- analysis/SymbolEntry.java
+++ analysis/SymbolEntry.java
@@ -1,43 +1,53 @@
 public class SymbolEntry extends Object{
 
-   
    public static final int NONE = 0;
    public static final int CONST = 1;
    public static final int PARAM = 2;
    public static final int PROC = 3;
    public static final int TYPE = 4;
    public static final int VAR = 5;
-   
+
    private String name;
    public int role;
-   public SymbolEntry next;
+   public SymbolEntry next=null;
 
    public SymbolEntry(String id){
       name = id;
       role = NONE;
       next = null;
    }
+   
+   public String getName(){
+      return name;
+   }
 
    public String toString(){
-      return "Name: " + name + "\n" + "Role: " + roleToString();
+		return "Name: " + name + "\n" + "Role: " + roleToString();
+	  
    }
 
    public void setRole(int r){
       role = r;
-      if (next != null)
+	  if(this.name.equals("")){// detecting emptySymbolEntry
+		  return;
+	  }
+      if (next != null){
          next.setRole(r);
+	  }
    }
 
    public void append(SymbolEntry entry){
-	   if (next == null)
-         next = entry;
-	     next.next = null;
-     
-	   else
-         next.append(entry);
+	 if(this.name.equals("")){// detecting emptySymbolEntry
+		 return;
+	 }
+	 if(next==null){
+		 next = entry;
+	 }else{
+		 next.append(entry);
+	 }
+
    }  
-  
-   
+
    private String roleToString(){
       String s = "";
       switch (role){
@@ -51,6 +61,5 @@ public class SymbolEntry extends Object{
       }
       return s;
    }
-  
 
 }
